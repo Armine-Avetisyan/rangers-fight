a5f084971bd6f34de80c813af7048ea10282462f
Armine <armineawtisian@gmail.com>
2021-07-19 19:21:17 +0400

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ranger } from './shared/interfaces';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rangers-fight';
+
+  ranger: Ranger[] = [
+    new Ranger('Ranger 1', 100, 100),
+    new Ranger('Ranger 2', 100, 100),
+
+]
+
+  fight() {
+    this.ranger[0].start();
+    this.ranger[1].start();
+  }
+  gameOver() {
+    this.ranger[0].finish();
+    this.ranger[1].finish();
+    setTimeout(() => {
+      console.log(`The winner is ${this.ranger[0].life > this.ranger[1].life ? this.ranger[0].name: this.ranger[1].name} with
+    ${Math.max(this.ranger[0].life, this.ranger[1].life)} %`)
+    }, 3000)
+
+  }
 }
--- src/app/shared/interfaces.ts
+++ src/app/shared/interfaces.ts
@@ -9,19 +9,38 @@
 
 abstract class Player {
   name: string;
-  age: number;
+  match: number;
   life: number;
-  protected constructor(name: string, age: number, life: number) {
+  protected constructor(name: string, match: number, life: number) {
     this.name = name;
-    this.age = age;
+    this.match = match;
     this.life = life;
   }
 
 }
 
 export  class Ranger extends Player{
-  constructor(name: string, age: number, life: number){
-    super(name, age, life)
+  constructor(name: string, match: number, life: number){
+    super(name, match, life)
+  }
+  rangerData() {
+    return {
+      name: this.name,
+      match: this.match,
+      life: this.life,
+    }
+  }
+
+  start() {
+    this.match = setInterval(() => {
+      this.life -= Math.trunc(Math.random() * 10 + 1);
+      console.log(`${this.name} - ${this.life}`)
+    }, 1000)
+
+  }
+
+  finish() {
+    clearInterval(this.match)
   }
 }
 
